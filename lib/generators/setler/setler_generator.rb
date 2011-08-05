5033b1c37a595e11920fd613bf0733c2137b8949
Chris Kelly <ckdake@ckdake.com>
2011-08-05 14:05:29 -0400

    Add next_migration_number method for generator

--- lib/generators/setler/setler_generator.rb
+++ lib/generators/setler/setler_generator.rb
@@ -7,6 +7,21 @@ class SetlerGenerator < Rails::Generators::NamedBase
   
   source_root File.expand_path('../templates', __FILE__)
   
+  @@migrations = false
+  
+  def self.next_migration_number(dirname)
+    if ActiveRecord::Base.timestamped_migrations
+      if @@migrations
+        (current_migration_number(dirname) + 1)
+      else
+        @@migrations = true
+        Time.now.utc.strftime("%Y%m%d%H%M%S")
+      end
+    else
+      "%.3d" % (current_migration_number(dirname) + 1)
+    end
+  end
+  
   def generate_model
     template "model.rb", File.join("app/models",class_path,"#{file_name}.rb"), force: true
     migration_template "migration.rb", "db/migrate/setler_create_#{table_name}.rb"
