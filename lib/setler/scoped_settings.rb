343741d520af7ef6e904e104ae2bbf6f95e5c1e8
rheaton <rachelmheaton@gmail.com>
2014-06-25 18:57:58 -0400

    rename method in scoped_settings and add a comment about why we can't use activesupport default constantice

--- lib/setler/scoped_settings.rb
+++ lib/setler/scoped_settings.rb
@@ -2,7 +2,7 @@ module Setler
   class ScopedSettings < Settings
     def self.for_thing(object, scopename)
       self.table_name = scopename
-      self.defaults = plural_constantize(scopename).defaults
+      self.defaults = settings_constantize(scopename).defaults
       @object = object
       self
     end
@@ -11,7 +11,9 @@ module Setler
       self.base_class.where(thing_type: @object.class.base_class.to_s, thing_id: @object.id)
     end
 
-    def self.plural_constantize(scopename)
+    # do not use rails default to singularize because setler examples
+    # user plural class names
+    def self.settings_constantize(scopename)
       Object.const_get(scopename.to_s.camelize)
     end
 
